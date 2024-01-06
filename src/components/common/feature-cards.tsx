dcf867d20ef8dacc7b295df37fabb1ffc7a2727f
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-01-06 23:29:33 +0000

    Fixes

--- src/components/common/feature-cards.tsx
+++ src/components/common/feature-cards.tsx
@@ -50,14 +50,16 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
         className="card w-full"
       >
         <div className={`body text-${textColor}`}>
-          <div className="flex flex-col items-start w-full md:w-[45%] justify-center md:-mt-[5rem] pl-5 pr-5 md:pl-20 md:pr-0 pt-10 md:pt-0">
+          <div className="flex flex-col items-start w-full md:w-[80%] lg:[70%] 2xl:w-[65%] 3xl:w-[50%] 4xl:w-[50%] justify-center md:-mt-[5rem] pl-5 pr-5 md:pl-20 md:pr-0 pt-10 md:pt-0">
             <div className="flex flex-row w-full gap-4">
               <p className="text-lg md:text-xl font-archivo">{number}</p>
               <h2 className="text-4xl md:text-5xl font-semibold font-cabinetGrotesk">
                 {title}
               </h2>
             </div>
-            <p className={`mt-1 md:mt-6 text-lg md:text-xl font-archivo font-normal opacity-80`}>
+            <p
+              className={`mt-1 md:mt-6 text-lg md:text-xl font-archivo font-normal opacity-80`}
+            >
               {description}
             </p>
             <div className="mt-10 hidden md:block">
